9b46180ea39f3c4e62d9d26d16d6671ec35c6ef2
Farjad Habib <habib.farjad@gmail.com>
2019-05-26 18:43:43 -0700

    AzureAD User Password Change

---
+++ AzureADUserPassword.ps1
@@ -0,0 +1,17 @@
+#connecting to the service
+Connect-MSOLservice
+
+#get Azure AD user by user id
+Get-MSOLUser -UserPrincipalName <user ID> | Select PasswordNeverExpires
+
+#displaying selective user properties
+Get-MSOLUser | Select UserPrincipalName, PasswordNeverExpires
+
+#set user properties
+Set-MsolUser -UserPrincipalName <user ID> -PasswordNeverExpires \$false
+
+#get user and then set user properties in bulk
+Get-MSOLUser | Set-MsolUser -PasswordNeverExpires \$false
+
+#setting new user password
+Set-MsolUserPassword -userPrincipalName "user@consoso.com" -NewPassword "new password"
